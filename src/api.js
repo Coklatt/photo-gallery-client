2026574d51bd2e727d62bd8473478ebb6b50c2da
reader00 <qoryamanahputra@gmail.com>
2024-05-05 19:50:09 +0700

    test

--- src/api.js
+++ src/api.js
@@ -1,28 +1,28 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const getImages = async (nextCursor) => {
-	const params = new URLSearchParams();
+    const params = new URLSearchParams();
 
-	if (nextCursor) {
-		params.append('next_cursor', nextCursor);
-	}
+    if (nextCursor) {
+        params.append('next_cursor', nextCursor);
+    }
 
-	const response = await fetch(`${API_URL}/photos?${params}`);
-	const responseJson = await response.json();
+    let response = await fetch(`${API_URL}/photos?${params}`);
+    response = await response.json();
 
-	return responseJson;
+    return response;
 };
 
 export const searchImages = async (searchValue, nextCursor) => {
-	const params = new URLSearchParams();
-	params.append(`expression`, searchValue);
+    const params = new URLSearchParams();
+    params.append(`expression`, searchValue);
 
-	if (nextCursor) {
-		params.append('next_cursor', nextCursor);
-	}
+    if (nextCursor) {
+        params.append('next_cursor', nextCursor);
+    }
 
-	const response = await fetch(`${API_URL}/search?${params}`);
-	const responseJson = await response.json();
+    let response = await fetch(`${API_URL}/search?${params}`);
+    response = await response.json();
 
-	return responseJson;
+    return response;
 };
